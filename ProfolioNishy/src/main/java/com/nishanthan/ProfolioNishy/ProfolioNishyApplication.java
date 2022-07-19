7a68b041900f2f3c926141a2a2475aaecae93386
nishanthan jana <nishanthan.janarthanarajah@rootcodelabs.com>
2022-07-19 22:55:44 +0530

    Initial commit

---
+++ ProfolioNishy/src/main/java/com/nishanthan/ProfolioNishy/ProfolioNishyApplication.java
@@ -0,0 +1,13 @@
+package com.nishanthan.ProfolioNishy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfolioNishyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfolioNishyApplication.class, args);
+	}
+
+}
